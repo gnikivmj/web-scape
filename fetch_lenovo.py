5d00b209ef6874efec92779a0b67af02a6567e91
gnikivmj <13000356@qq.com>
2014-10-30 10:48:18 -0700

    init

---
+++ fetch_lenovo.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+import codecs
+import time
+from pyvirtualdisplay import Display
+
+url = "http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&page-size=100&facet-1=3"
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+driver = webdriver.Firefox()
+#driver.manage().window().setPosition(new Point(-2000, 0));
+#driver.get("http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&facet-2=1&page-size=100&facet-1=2&facet-3=14")
+#driver.get("http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&page-size=100&facet-3=14&facet-8=9&sort-criteria=2&facet-1=1&facet-1=3")
+driver.get(url)
+#assert "Python" in driver.title
+#elem = driver.find_elemen:t_by_name("q")
+#elem.send_keys("pycon")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
+#driver.implicitly_wait(30)
+driver.set_window_size(1000,1000)
+i = 0
+for i in range(0,18):
+#  i += 1
+  time.sleep(4)
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+f = codecs.open("a.html", "w", "utf-8")
+f.write(driver.page_source)
+f.close()
+driver.close()
---
+++ parse_lenovo.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+"""A tiny Python program to check that Python is working.
+Try running this program from the command line like this:
+  python hello.py
+  python hello.py Alice
+That should print:
+  Hello World -or- Hello Alice
+Try changing the 'Hello' to 'Howdy' and run again.
+Once you have that working, you're ready for class -- you can edit
+and run Python code; now you just need to learn Python!
+"""
+
+import sys
+import requests
+import codecs
+from lxml import html
+
+#
+# Define a main() function that prints a little greeting.
+def main():
+  # Get the name from the command line, using 'World' as a fallback.
+  if len(sys.argv) >= 2:
+    name = sys.argv[1]
+  else:
+    name = 'World'
+  print 'Hello', name
+#  sys.setdefaultencoding('utf-8')
+  f = codecs.open('a.html', 'r', 'utf-8')
+#  print f.read()
+  tree = html.fromstring(f.read())
+  tab_select = '//div[@class="facet-result cmpr_listing"]'
+  li = tree.xpath(tab_select)
+  print tab_select
+  j = 0
+  for items in li:
+    j += 1
+    type = items[1].find('div/h1/a').text_content().strip()
+    if 'T440s' not in type:
+      continue
+
+    prices = items[2].find('div[@class="pricing"]/dl/dd[@class="aftercoupon value"]')#/dl/dd[@class="ftercoupon value"]')
+    #if float(prices.text_content().strip().replace('$,', '')) > 850:
+    #  continue
+      
+    print type
+    print items[1].find('div[@class="fbr-partnum"]').text_content().strip()
+    features = items[1].find('ul[@class="fbr-features"]')
+    i = 0;
+    for feature in features:
+      i = i + 1
+      if i == 1:
+        continue
+      print " ".join(feature.text_content().strip().encode('ascii','ignore').split())
+    prices = items[2].find('div[@class="pricing"]/dl/dd[@class="aftercoupon value"]')#/dl/dd[@class="ftercoupon value"]')
+    print prices.text_content().strip()
+    print '-----'
+  print 'total count ', j
+#  print 
+#  print li
+#  li = tree.xpath(tab_select)
+#  for items in li:
+#    if items[UID].text_content().strip() == user:
+#      print 'gid: ' + items[GID].text_content().strip()+ ' status: ' + items[STATUS].text_content().strip()
+#      print 'title: ' + items[SUBJECT].text_content().strip()
+#      print '------------------'
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
+
