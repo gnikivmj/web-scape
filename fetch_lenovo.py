845f1c633976978fe2b05d920303b2831a2d9fda
gnikivmj <13000356@qq.com>
2014-10-30 20:54:42 -0700

    put everthing into function
    add while loop

--- fetch_lenovo.py
+++ fetch_lenovo.py
@@ -1,25 +1,82 @@
+#!/usr/bin/python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from pyvirtualdisplay import Display
+from lxml import html
+
 import sys
 import codecs
 import time
-from pyvirtualdisplay import Display
+import requests
+import re
 
+it = 18
+sleep_time = 300
 url = "http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&page-size=100&facet-1=3&sort-criteria=2"
+
+def fetch_content():
+  display = Display(visible=0, size=(800, 600))
+  display.start()
+
+  driver = webdriver.Firefox()
+  driver.get(url)
+  driver.set_window_size(1000,1000)
+  for i in range(0, it):
+    time.sleep(4)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#  f = codecs.open("a.html", "w", "utf-8")
+#  f.write(driver.page_source)
+#  f.close()
+  content = driver.page_source
+  driver.close()
+  display.stop()
+  return content
+
+def parse_content(content):
+  tree = html.fromstring(content)
+  tab_select = '//div[@class="facet-result cmpr_listing"]'
+  li = tree.xpath(tab_select)
+  print tab_select
+  j = 0
+  for items in li:
+    j += 1
+    model = items[1].find('div/h1/a').text_content().strip()
+    if 'T440s' not in model and 'Carbon' not in model:
+      continue
+
+    prices = items[2].find('div[@class="pricing"]/dl/dd[@class="aftercoupon value"]')#/dl/dd[@class="ftercoupon value"]')
+    p = re.sub('[$,]', '', prices.text_content().strip())
+    if float(p) > 900:
+      continue
+
+    print model
+    print items[1].find('div[@class="fbr-partnum"]').text_content().strip()
+    features = items[1].find('ul[@class="fbr-features"]')
+    for feature in features[1:]:
+      print " ".join(feature.text_content().strip().encode('ascii','ignore').split())
+    prices = items[2].find('div[@class="pricing"]/dl/dd[@class="aftercoupon value"]')#/dl/dd[@class="ftercoupon value"]')
+    print prices.text_content().strip()
+    print '-----'
+  print 'total count ', j
+  print '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+#
+  return 0
+
+if __name__ == "__main__":
+   while True:
+    print "fetch content ..."
+    content = fetch_content()
+    print "parsing ...", time.ctime()
+    parse_content(content)
+    print time.ctime(), "sleep for %s" % sleep_time
+    time.sleep(sleep_time)
+"""
 display = Display(visible=0, size=(800, 600))
 display.start()
 
 driver = webdriver.Firefox()
-#driver.manage().window().setPosition(new Point(-2000, 0));
-#driver.get("http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&facet-2=1&page-size=100&facet-1=2&facet-3=14")
-#driver.get("http://outlet.lenovo.com/outlet_us/laptops/#/?page-index=1&page-size=100&facet-3=14&facet-8=9&sort-criteria=2&facet-1=1&facet-1=3")
 driver.get(url)
-#assert "Python" in driver.title
-#elem = driver.find_elemen:t_by_name("q")
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-#driver.implicitly_wait(30)
 driver.set_window_size(1000,1000)
 i = 0
 for i in range(0,18):
@@ -32,3 +89,4 @@ f.write(driver.page_source)
 f.close()
 driver.close()
 display.stop()
+"""
